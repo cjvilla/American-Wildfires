e911f7bda2037422fe1aab6ed9255c0edaa02db3
Alec Miller <alexander.miller.31344@gmail.com>
2021-04-28 19:24:33 -0500

    shell flask app, index html, and basic pandas

---
+++ alec-work/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    # add in data variables here
+    return render_template("index.html")
+
+# for additional pages
+# @app.route("/api/v1.0/justice-league")
+# def justice_league():
+#     """Return the justice league data as json"""
+
+#     return jsonify(justice_league_members)
+
+if __name__ == "__main__":
+    app.run(debug=True)
