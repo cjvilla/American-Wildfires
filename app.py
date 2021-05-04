aa1ad0ab04e31e5fac02b6f05203566408de52d7
cjvilla <75910250+cjvilla@users.noreply.github.com>
2021-05-04 02:44:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ from sqlalchemy import create_engine
 import sqlite3 as lite
 import pandas as pd
 import json 
+import folium
+from folium import plugins
+import ipywidgets
+import geocoder
+import geopy
+import plotly.express as px
+
 
 app = Flask(__name__)
 
@@ -12,25 +19,24 @@ def welcome():
     return render_template("index.html")
 
 
-
 @app.route('/totalStateFires')
 #query for causes and total count of fires
 def total_fires():
     engine = create_engine("sqlite:///Resources/wildfires.sqlite")
-    con = lite.connect('Resources/wildfires.sqlite')
+    conn = lite.connect('Resources/wildfires.sqlite')
     total_query = 'select STATE, SUM(FIRE_YEAR) as "Total Fires", AVG(FIRE_SIZE) as "Average Size" from Fires group by State order by "Total Fires" desc;'
-    total_fires_df = pd.read_sql_query(total_query,con)
+    total_fires_df = pd.read_sql_query(total_query,conn)
     total_fires_ranked = {
         "state": total_fires_df["STATE"].values.tolist(),
         "fire_count": total_fires_df["Total Fires"].values.tolist(),
         "avg_size": total_fires_df["Average Size"].values.tolist(),
     }
-    con.close()
+    conn.close()
     return jsonify(total_fires_ranked)
 
-@app.route("/AcresBurned")
-def acres_burned():
-    return 'Hello World'
+@app.route('/causes_map')
+def causes_map():
+    return render_template('causes_map.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
